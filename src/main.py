deffa16a0f08b038a51b0a2cac68338c2a2fc971
Pedro Palma <pedrolucaspalma@gmail.com>
2022-11-26 19:14:30 -0300

    Begin adding more basic python code

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from models.Country import Country as Country
 
 # Variables for read_csv
 original_table = '../databases/Summer-Olympic-medals-1976-to-2008.csv'
@@ -8,10 +9,15 @@ encoding='latin-1'
 # Reading the csv data
 original_csv_df = pd.read_csv(original_table, usecols=original_cols, encoding=encoding )
 
-# Filtering data to only contain rows from Beijiing 2008 Olympics
+# Filtering data to only contain rows from Beijing 2008 Olympics
 beijing_data = original_csv_df[original_csv_df['Year'] == 2008]
 
 # Grouping the rows so we can get a count of medals for each country by each discipline
 beijing_data_country_sum = beijing_data.groupby(by=['Discipline','Country'])["Medal"].count().reset_index()
+
+
+
+
+print()
 
 # for index, row in beijing_data_country_sum.iterrows():
---
+++ src/models/Country.py
@@ -0,0 +1,7 @@
+class Country:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}"
+        
