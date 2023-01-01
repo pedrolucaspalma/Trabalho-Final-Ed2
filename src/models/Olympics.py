9c5cb841049543d1142ea2963a61eb99e0b55784
Pedro Palma <pedrolucaspalma@gmail.com>
2023-01-01 11:59:18 -0300

    Add comments

--- src/models/Olympics.py
+++ src/models/Olympics.py
@@ -1,15 +1,25 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def return_name_from_object(obj):
-    return obj.name
 class Olympics:
+        """ 
+        Classe base para armazenar os dados do grafo em memória.
+
+        A implementação do grafo foi feita através de uma matriz de adjacências, em que a classe Olympics
+        possui duas listas que armazenam os nós (que são instâncias de objetos das classes Country e Discipline).
+
+        Dessa forma, cada nó (Country ou Discipline) possui uma lista de Results, que representa quantas medalhas cada país recebeu naquela modalidade (no grafo, são arestas com pesos). 
+
+        Possui métodos para buscar, adicionar e remover nós, além de uma visualização gráfica com as bibliotecas networkx e pyplot. 
+        """
         def __init__(self):
             self.countries = [];
             self.disciplines = [];
             self.results = []
 
         def add_country(self, country):
+            """
+            """
             self.countries.append(country);
 
         def add_discipline(self, discipline):
@@ -19,10 +29,27 @@ class Olympics:
             self.results.append(result)
 
         def find_country(self, country_name):
+            """
+            Parameters
+            ----------
+            country_name: str
+                O nome do país
+
+            Busca simples em Array para encontrar um país a partir da classe Olympics.
+
+            """
             for country in self.countries:
                 if country.name == country_name: return country
 
         def find_discipline(self, discipline_name):
+            """
+            Parameters
+            ----------
+            discipline_name: str
+                O nome da modalidade
+
+            Busca simples em Array para encontrar uma modalidade a partir da classe Olympics
+            """
             for discipline in self.disciplines:
                 if discipline.name == discipline_name: return discipline
 
