fdb307085a94082790052df235f6b674c910616e
Pedro Palma <pedrolucaspalma@gmail.com>
2023-01-31 20:04:56 -0300

    delete commented code

--- src/displaying_algorithms.py
+++ src/displaying_algorithms.py
@@ -1,20 +1,3 @@
-
-#def run_algorithms(olympics):
-   # print("========Medalhas do Egito========")
-   # country1 = olympics.find_node_bfs("Egypt")
-    #for result in country1.results:
-        #print(result)
-
-   # print("========Medalhas do Brasil========")
-   # country2 = olympics.find_node_bfs("Brazil")
-    #for result in country2.results:
-        #print(result)
-
-   # print("========Países que competiram no Arco e Flecha========")
-    #discipline1 = olympics.find_node_bfs("Archery")
-    #for result in discipline1.results:
-       # print(result)
-
 def menu(olympics):
     discipline_name: str = ""
     country_name: str = ""
